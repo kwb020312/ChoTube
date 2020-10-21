d66f998a8823cd9addc1941ee1442dfaf8cdfa73
kimwoobin <kwb020312@naver.com>
2020-10-21 22:42:04 +0900

    Video Detail Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -22,23 +22,29 @@ function LandingPage() {
     const renderCards = Video.map((video , i) => {
         let minutes = Math.floor(video.duration/60)
         let seconds = Math.floor((video.duration -  minutes*60))
-        return (
-            <Col lg={6} md={8} xs={24} key={i}>
-                <a href={`/video/post/${video._id}`}>
-                    <div style={{position:'relative'}}>
-                        <img style={{width:'100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thunmbnail" />
-                        <div className="duration">
-                            <span>{minutes} : {seconds}</span>
+        if(video.writer) {
+            return (
+                <Col lg={6} md={8} xs={24} key={i}>
+                    <a href={`/video/${video._id}`}>
+                        <div style={{position:'relative'}}>
+                            <img style={{width:'100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thunmbnail" />
+                            <div className="duration">
+                                <span>{minutes} : {seconds}</span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-                <br/>
-                <Meta avatar={<Avatar src={video.writer.image} />} title={video.title} description="" />
-                <span>{video.writer.name}</span>
-                <br />
-        <span style={{marginLeft:'3rem' }}>{video.views} views</span> - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
-            </Col>
-        )
+                    </a>
+                    <br/>
+                    <Meta avatar={<Avatar src={video.writer.image} />} title={video.title} description="" />
+                    <span>{video.writer.name}</span>
+                    <br />
+            <span style={{marginLeft:'3rem' }}>{video.views} views</span> - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+                </Col>
+            )
+        } else {
+            return (
+                null
+            )
+        }
     })
 
     return (
---
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -0,0 +1,43 @@
+import { Avatar, Col, List, Row } from 'antd'
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export default function VideoDetailPage(props) {
+    const [VideoDetail , setVideoDetail] = useState([])
+    useEffect(() => {
+        const videoId = props.match.params.videoId
+        const variable = {
+            videoId
+        }
+        Axios.post('/api/video/getVideoDetail',variable)
+        .then(response => {
+            if(response.data.success) {
+                setVideoDetail(response.data.videoDetail)
+            } else {
+                alert('비디오 정보를 가져오지 못했습니다.')
+            }
+        })
+    },[])
+
+    if(VideoDetail.writer) {
+        return (
+            <Row gutter={[16, 16]}>
+                <Col lg={18} xs={24}>
+                    <div style={{width: '100%' , padding: '3rem 4rem'}}>
+                        <video style={{width:'100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
+                        <List.Item actions>
+                            <List.Item.Meta avatar={<Avatar src={VideoDetail.writer.image}/>} title={VideoDetail.writer.name} description={VideoDetail.description} />
+                        </List.Item>
+                    </div>
+                </Col>
+                <Col lg={6} xs={24}>
+                    Side Videos
+                </Col>
+            </Row>
+        )
+    } else {
+        return (
+            <div>...loading</div>
+        )
+    }
+}
