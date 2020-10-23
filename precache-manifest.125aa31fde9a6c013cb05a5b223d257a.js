d90260d177ccb6bae019d9a696639a41c21304a7
kimwoobin <kwb020312@naver.com>
2020-10-23 20:21:06 +0900

    Updates

---
+++ precache-manifest.125aa31fde9a6c013cb05a5b223d257a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c7e7b89cffd309e0dac748c92c03a6",
+    "url": "/ChoTube/index.html"
+  },
+  {
+    "revision": "d44ac584530d82b52778",
+    "url": "/ChoTube/static/css/2.630594e3.chunk.css"
+  },
+  {
+    "revision": "a459131cdf88da6afd20",
+    "url": "/ChoTube/static/css/main.87a4097b.chunk.css"
+  },
+  {
+    "revision": "d44ac584530d82b52778",
+    "url": "/ChoTube/static/js/2.c7b1b0de.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/ChoTube/static/js/2.c7b1b0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a459131cdf88da6afd20",
+    "url": "/ChoTube/static/js/main.65bda2fd.chunk.js"
+  },
+  {
+    "revision": "17762f5c11a943f8c394",
+    "url": "/ChoTube/static/js/runtime-main.008a73cb.js"
+  }
+]);
